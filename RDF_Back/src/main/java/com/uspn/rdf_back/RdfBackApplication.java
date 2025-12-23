c8edbd7487e9dbcd69123bd1ac2513d1724191b3
Saber Mahjoub <saber.mahjoub003@gmail.com>
2025-12-23 12:10:59 +0100

    Initial backend setup

---
+++ RDF_Back/src/main/java/com/uspn/rdf_back/RdfBackApplication.java
@@ -0,0 +1,13 @@
+package com.uspn.rdf_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdfBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdfBackApplication.class, args);
+    }
+
+}
---
+++ RDF_Back/src/test/java/com/uspn/rdf_back/RdfBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uspn.rdf_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdfBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
