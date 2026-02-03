3bdca94a75649ce76f50e13a276b3ebfbf8a4f7c
Saber Mahjoub <saber.mahjoub003@gmail.com>
2026-02-03 20:42:46 +0100

    Angular setup and installations

---
+++ Frontend/src/app/components/gestion-ressources/gestion-ressources.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-ressources',
+  imports: [],
+  templateUrl: './gestion-ressources.component.html',
+  styleUrl: './gestion-ressources.component.scss'
+})
+export class GestionRessourcesComponent {
+
+}
