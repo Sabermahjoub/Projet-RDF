384fdb489162cbc383b37b07e9e01e7bc5dc06f9
Saber Mahjoub <saber.mahjoub003@gmail.com>
2026-02-03 20:42:46 +0100

    Angular setup and installations

---
+++ Frontend/src/app/components/gestion-sources/gestion-sources.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-sources',
+  imports: [],
+  templateUrl: './gestion-sources.component.html',
+  styleUrl: './gestion-sources.component.scss'
+})
+export class GestionSourcesComponent {
+
+}
