1f33cf1903a1efe5d227124ebfa403960c8855a1
Saber Mahjoub <saber.mahjoub003@gmail.com>
2026-03-04 15:14:49 +0100

    Primar version of gestion ressources

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { GestionProjetsComponent } from './components/gestion-projets/gestion-projets.component';
 import { Route } from '@angular/router';
+import { GestionRessourcesComponent } from './components/gestion-ressources/gestion-ressources.component';
 
 
 export const routes: Route[] = [
+  {
+    path: 'GestionRessources',
+    component: GestionRessourcesComponent
+  },
   {
     path: 'GestionProjets',
     component: GestionProjetsComponent,
