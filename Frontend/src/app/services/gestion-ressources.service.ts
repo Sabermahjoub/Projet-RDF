b83443556d0b7d1bf1c9f2c31b650ee02b47211c
Saber Mahjoub <saber.mahjoub003@gmail.com>
2026-03-25 16:38:21 +0100

    Added a service to get all entities by type

--- Frontend/src/app/services/gestion-ressources.service.ts
+++ Frontend/src/app/services/gestion-ressources.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ONTOLOGY_LABELS } from '../models/ontology-labels';
 
@@ -9,6 +9,7 @@ import { ONTOLOGY_LABELS } from '../models/ontology-labels';
 export class GestionRessourcesService {
   private apiUrl = 'http://localhost:8080/sparql';
   private ontologyUrl = 'http://localhost:8080/ontology';
+  private rdfUrl = 'http://localhost:8080/rdf';
   constructor(private http: HttpClient) { }
 
   getOntologyLabel(listeIris : string[] ) : any[] {
@@ -54,6 +55,15 @@ export class GestionRessourcesService {
     return this.http.get<any>(`${this.ontologyUrl}/types`);  
   }
 
+  getTypeUrlByName(ontologyName: string): string {
+    const entry = Object.entries(ONTOLOGY_LABELS).find(([_, label]) => label === ontologyName);
+    return entry ? entry[0] : '';
+  }
+
+  getAllEntitiesByType(typeUrl: string): Observable<any> {
+    const params = new HttpParams().set('type', typeUrl);
+    return this.http.get<any>(`${this.rdfUrl}/entities`, {params});
+  }
   // getTypes(): Observable<any> {
 
   //   const object = {
